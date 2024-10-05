cbc866ed541ea3b0c0e153db325f724c98dd7e5a
Nikolay Valchanov <nv87121557@edu.mon.bg>
2024-10-05 17:08:51 +0300

    Initial Setup

---
+++ sbojWebApp/sbojWebApp/Models/Language.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sbojWebApp.Models
+{
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
