f9666913b50dba07eb9eaae12e5888c7777ed286
Nikolay Valchanov <nikolay.valchanov09@gmail.com>
2024-10-08 22:53:35 +0300

    Updated Pagination in City/Index

--- sbojWebApp/sbojWebApp/obj/Debug/net8.0/sbojWebApp.AssemblyInfo.cs
+++ sbojWebApp/sbojWebApp/obj/Debug/net8.0/sbojWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sbojWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09862c087ebdf8b9715d8a70cceb87bc00e580a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cbf2ff7f755a2add3e24dacb99c90eff993418c")]
 [assembly: System.Reflection.AssemblyProductAttribute("sbojWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sbojWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
