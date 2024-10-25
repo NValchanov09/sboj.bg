562d50a981b9c4b2d50e56d54871c9542cee4a66
Nikolay Valchanov <nikolay.valchanov09@gmail.com>
2024-10-25 22:25:43 +0300

    New Edit page for Companies

--- sbojWebApp/sbojWebApp/obj/Debug/net8.0/sbojWebApp.AssemblyInfo.cs
+++ sbojWebApp/sbojWebApp/obj/Debug/net8.0/sbojWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sbojWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257a426b2036bd7defab36d06f9178dd68d74ff6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9855d626a061f2d4051db6b3b3b6b6e58c1ba708")]
 [assembly: System.Reflection.AssemblyProductAttribute("sbojWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sbojWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
